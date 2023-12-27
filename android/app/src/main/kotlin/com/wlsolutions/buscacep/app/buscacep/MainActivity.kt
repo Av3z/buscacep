dce07d3ef2e055837a7d51f837d11711931d5ed3
Av3z <willyanf17@hotmail.com>
2023-12-26 23:15:10 -0300

    initial config

---
+++ android/app/src/main/kotlin/com/wlsolutions/buscacep/app/buscacep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wlsolutions.buscacep.app.buscacep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:buscacep/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
