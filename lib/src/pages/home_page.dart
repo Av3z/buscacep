ca53606d0e824023c9d3362a533aba32478e60bb
Av3z <willyanf17@hotmail.com>
2024-01-03 13:09:44 -0300

    feat: adde texts for show result cep

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,26 @@
+import 'package:buscacep/src/models/cep_model.dart';
+import 'package:buscacep/src/repositories/cep_repository.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CEPRepository cepRepository = CEPRepository();
+  TextEditingController cepController = TextEditingController();
+  CEPModel cep = CEPModel();
+  bool loading = false;
+
+  void onPressed() async {
+    loading = true;
+    cep = await cepRepository.getCep(cepController.text);
+    setState(() {});
+    loading = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +30,10 @@ class HomePage extends StatelessWidget {
           const Text(
             'BuscaCEP',
             style: TextStyle(
-                color: Colors.green, fontWeight: FontWeight.bold, fontSize: 35),
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+                fontSize: 35,
+                fontStyle: FontStyle.italic),
           ),
           const SizedBox(
             height: 60,
@@ -19,6 +41,7 @@ class HomePage extends StatelessWidget {
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 10),
             child: TextField(
+              controller: cepController,
               keyboardType: TextInputType.number,
               maxLength: 8,
               decoration: InputDecoration(
@@ -54,7 +77,7 @@ class HomePage extends StatelessWidget {
                     shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10)))),
-                onPressed: () {},
+                onPressed: onPressed,
                 child: const Text(
                   'Buscar',
                   style: TextStyle(
@@ -62,7 +85,34 @@ class HomePage extends StatelessWidget {
                       fontSize: 18,
                       fontWeight: FontWeight.bold),
                 )),
-          )
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(cep.localidade ?? '',
+                  style: TextStyle(
+                      color: Colors.grey[900],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18)),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(cep.uf ?? '',
+                  style: TextStyle(
+                      color: Colors.grey[900],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18)),
+            ],
+          ),
+          Text(
+            cep.logradouro ?? '',
+            style: TextStyle(color: Colors.grey[900]),
+          ),
+          Text(cep.bairro ?? '', style: TextStyle(color: Colors.grey[900])),
+          if (loading) const CircularProgressIndicator(),
         ]),
       ),
     );
